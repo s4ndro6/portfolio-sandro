7a0bb04eb451c8c553dd8390bd0bef5dadc831ad
s4ndro6 <sandro136084@gmail.com>
2026-02-02 14:59:57 +0100

    FINAL UI STABILIZATION: CDN Logos & Speed Tuning

--- src/components/ProjectCarousel.jsx
+++ src/components/ProjectCarousel.jsx
@@ -37,7 +37,7 @@ const ProjectCarousel = () => {
                         x: {
                             repeat: Infinity,
                             repeatType: "loop",
-                            duration: 120, // Very slow smooth scroll (Confirmed 120s)
+                            duration: 150, // Ultra slow smooth scroll (Confirmed 150s)
                             ease: "linear",
                         },
                     }}
